6d53739ed9e8606fbfbc53e39b3213348e498638
Mr. Park <72147590+mrparkyrdsb@users.noreply.github.com>
2023-03-27 16:40:20 +0000

    added Average.java

---
+++ Average.java
@@ -0,0 +1,14 @@
+class Average {
+    public static void main(String[] args) {
+        System.out.println("Average Calculator");
+
+        int mark1 = 52;
+        int mark2 = 91;
+        int mark3 = 70;
+        int mark4 = 82;
+
+        double average = (mark1 + mark2 + mark3 + mark4) / 4;
+
+        System.out.println("Your average is: " + average);
+    }
+}
